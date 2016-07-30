9752fbef1d77894bd15b62c9ec4b445afd562dbc
chrisalfonso <chrisalfonso@users.noreply.github.com>
2016-07-29 22:23:27 -0400

    Create tournament.py

---
+++ tournament.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    query = "DELETE FROM matches"
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    query = "DELETE FROM players"
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    query = "SELECT count(*) FROM players"
+    c.execute(query)
+    player_count = c.fetchall()
+    conn.close()
+    return player_count[0][0]
+
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+
+    NOTE:
+      Unsure why the countPlayers() function is returned at the end, but it
+      seems to be needed. 'name' not defined error otherwise.
+
+    """
+    conn = connect()
+    c = conn.cursor()
+    query = "INSERT INTO players (name) VALUES (%s)"
+    data = (name,)
+    c.execute (query, data)
+    conn.commit()
+    conn.close()
+    return countPlayers()
+
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+
+    # I'll use subqueries (or views?) for the counts of wins and matches
+    # Need to fill the db with some test data and run a few queries
+    
+    # SUBQUERY METHOD
+    #num_wins = select count(wins) from 
+	#(select outcome from matches where outcome = "winner") 
+	#as wins
+    #num_matches = select count(rounds_played) from
+	#(select rounds from matches where players.id = matches.id
+	#as rounds_played
+
+# TO DO: create a view
+    
+    conn = connect()
+    c = conn.cursor()
+    query = "select players.id, players.name, (select count(matches.outcome) from matches,players where players.id = matches.id and outcome = 'winner') as wins, (select count(matches.round) from matches,players where players.id = matches.id and outcome='winner' or outcome='loser') as matches from players order by wins desc, matches desc"
+    c.execute(query)
+    player_records = c.fetchall()
+    conn.commit()
+    conn.close()
+    return player_records
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+
+    conn = connect()
+    c = conn.cursor()
+    query = "INSERT INTO matches (winner, loser) VALUES (%s, %s)"
+    data = ()
+    c.execute(query, data)
+    conn.commit()
+    conn.close()
+ 
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
